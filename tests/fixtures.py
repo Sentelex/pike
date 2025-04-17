5027274bc8e0be39feb769a2df9b091019d71c28
Renat Khaliullin <renatkh@gmail.com>
2025-04-17 15:29:47 -0600

    feat: added mock model
    feat: rewrote graph test to check for tool calls

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,26 +1,14 @@
 import pytest
 import langchain_core.messages as lcm
 import src.state as st
+import src.mock_model as mock
+import langchain_core.tools as lcct
 
 
 @pytest.fixture
 def mock_message():
     """Fixture to provide a mock message."""
     return lcm.BaseMessage(content="Test message", type="text")
-
-
-@pytest.fixture
-def mock_model():
-    """Fixture to provide a mock model with a bind_tools() function."""
-    class MockModel:
-        def __init__(self):
-            self.tools = []
-
-        def bind_tools(self, tools):
-            """Mock implementation of bind_tools."""
-            self.tools = tools
-
-    return MockModel()
 
 
 @pytest.fixture
