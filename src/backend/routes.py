ee17127fc2d34fa5a3e7ec6a21e57fce35180c25
Justin Hooper <jhooperonline@gmail.com>
2025-04-27 16:30:41 -0600

    feat:  Added more routes for the API.

--- src/backend/routes.py
+++ src/backend/routes.py
@@ -3,6 +3,7 @@ import typing as t
 import pydantic as pyd
 import langchain_core.messages as lcm
 import fastapi as fapi
+import copy
 
 import backend.data_specs.agent_spec as agent
 import backend.data_specs.chat_spec as chat
@@ -35,7 +36,20 @@ class InputMessage(pyd.BaseModel):
 
 # User access calls
 
+
 ## Read calls
+# c.iii.1 (?) get available agents
+@pike.api.get("/agents")
+async def get_public_agents() -> list[agent.Agent]:
+    """
+    Get a list of all public agent types.
+
+    Returns
+    -------
+    agent_list: list[Agent]
+    """
+    return list(teda.all_agent_set)
+
 # 1.d.  User settings
 @pike.api.get("/user/{user_id}")
 async def get_user_info(user_id: user.UserID) -> user.User:
@@ -74,6 +88,27 @@ async def get_user_agents(user_id: user.UserID) -> list[agent.Agent]:
     agent_list = teda.mickey_user.agents
     return agent_list
 
+# 1.b.  get compact chat representations
+@pike.api.get("/user/{user_id}/chats")
+async def get_user_chats_compact(user_id: user.UserID) -> list[chat.ChatTag]:
+    """
+    Gets a list of chat tags, providing enough information to render the associated
+    chats without messages.
+
+    Parameters:
+    -----------
+    user_id:  UserToken
+      User's ID token from login
+
+    Returns:
+    --------
+    compact_chat_list: list[ChatTag]
+      A list of compact representations (ChatTags) of chats in which the user has been
+      involved
+    """
+    compact_chat_list = teda.mickey_user.chats
+    return compact_chat_list
+
 # 2.a
 @pike.api.get("/user/{user_id}/agent/{agent_id}/chats")
 async def get_user_chats_by_agent(user_id: user.UserID, agent_id: agent.AgentID)->list[chat.ChatTag]:
@@ -99,27 +134,6 @@ async def get_user_chats_by_agent(user_id: user.UserID, agent_id: agent.AgentID)
     # chat_list = [chat for chat in user_chats if chat.agent.ID == agent_id]
     chat_list = [teda.mickey_Chat1.tag]
     return chat_list
-
-# 1.b.  get compact chat representations
-@pike.api.get("/user/{user_id}/chats")
-async def get_user_chats_compact(user_id: user.UserID) -> list[chat.ChatTag]:
-    """
-    Gets a list of chat tags, providing enough information to render the associated
-    chats without messages.
-
-    Parameters:
-    -----------
-    user_id:  UserToken
-      User's ID token from login
-
-    Returns:
-    --------
-    compact_chat_list: list[ChatTag]
-      A list of compact representations (ChatTags) of chats in which the user has been
-      involved
-    """
-    compact_chat_list = teda.mickey_user.chats
-    return compact_chat_list
 
 # 2.b (somwhat) get a chat's full history
 @pike.api.get("/user/{user_id}/chat/{chat_id}")
@@ -157,18 +171,6 @@ async def get_user_chat(user_id: user.UserID, chat_id: chat.ChatID) -> chat.Chat
     # except Exception as e:
     #     raise
 
-# c.iii.1 (?) get available agents
-@pike.api.get("/agents")
-async def get_public_agents() -> list[agent.Agent]:
-    """
-    Get a list of all public agent types.
-
-    Returns
-    -------
-    agent_list: list[Agent] 
-    """
-    return list(teda.all_agent_set)
-
 # 2.h Get attachment information.  (Should be implicitly filtered by user as attachments are in user's chats)
 @pike.api.get("/attachment/{attachment_id}")
 async def get_attachment(attachment_id: attach.AttachmentID) -> attach.Attachment:
@@ -180,6 +182,19 @@ async def get_attachment(attachment_id: attach.AttachmentID) -> attach.Attachmen
     attachment_id:  The UUID of the attachment requested.
     """
     return teda.bbgun_Attach
+
+@pike.api.get("/agent/{agent_id}")
+async def get_agent(agent_id: agent.AgentID)-> agent.Agent:
+    """
+    Retrieve the information about a specific agent.
+
+    Parameters
+    ----------
+    agent_id: UUID
+      The ID of the agent about which to retrieve information.
+    """
+
+    return teda.default_Agent
 
 ## Create calls
 # 2.c Create a new chat
@@ -218,8 +233,6 @@ async def create_chat(user_id: user.UserID, agent_id: agent.AgentID)-> chat.Chat
     )
     return new_chatTag
 
-
-## Creation calls
 # c.ii.  add agent (Ignore agent preparedness flag for now)
 @pike.api.post("/user/{user_id}/agent/{agent_id}")
 async def add_agent_to_user(
@@ -265,3 +278,71 @@ async def invoke_chat(user_id: user.UserID, chat_id: chat.ChatID, input: InputMe
   except Exception as err:
     print(f"Error in invoke_chat: {str(err)}")
     raise fapi.HTTPException(status_code=500, detail=str(err))
+
+##Deletion calls
+# Delete agent from user
+@pike.api.delete("/user/{user_id}/agent/{agent_id}")
+async def remove_agent_from_user(user_id: user.UserID, agent_id: agent.AgentID) -> list[agent.Agent]:
+    """
+    Removes a curent agent from the user's current agent list.
+
+    Parameters
+    ----------
+    user_id: UserToken
+      User's ID token from login
+    agent_id: UUID
+      ID of the agent to remove from the user's list
+
+    Returns:
+    --------
+    new_agent_list:  list[Agent]
+      Adjusted list of agents for the user
+    """
+    if teda.additional_Agent in teda.mickey_user.agents:
+        teda.mickey_user.agents.pop(teda.additional_Agent)
+    return teda.mickey_user.agents
+
+@pike.api.delete("/user/{user_id}/chat/{chat_id}")
+async def remove_chat_from_user(user_id: user.UserID, chat_id: chat.ChatID)->list[chat.ChatTag]:
+    """
+    Deletes a chat and all associated information, unlinking it from a user's history.
+
+    Parameters
+    ----------
+    user_id : UserToken
+      The chat owner's ID
+    chat_id : UUID
+      The chat identifier of the chat to be deleted.
+
+    Returns
+    -------
+      The updated list of chat tags belonging to the user after deletion.
+    """
+
+    return copy.copy(teda.mickey_user_chats).pop(teda.mickey_ChatTag_2)
+
+##Put calls
+# Change chat flags
+@pike.api.put("/user/{user_id}/chat/{chat_id}")
+async def modify_chat_status(user_id: user.UserID, chat_id: chat.ChatID, chat_flags: chat.ChatFlags)->chat.ChatTag:
+    """
+    Modifies the flags controlling a chat's state (pinned, bookmarked, open, etc...)
+
+    Parameters
+    ----------
+    user_id : UserToken
+      ID of the user owning the chat
+    chat_id : UUID
+      Identifier for the chat being modified
+    chat_flags : ChatFlags
+      The flags and their respective desired states
+
+    Returns
+    -------
+      The updated ChatTag for the chat whose flags were modified.
+    """
+    
+    return (chat.ChatTag(teda.mickey_Chat1.tag.ID,
+                         teda.mickey_Chat1.tag.name,
+                         chat_flags,
+                         teda.mickey_Chat1.tag.agent))
