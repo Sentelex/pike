45fb153eaa9076c78ca8c68c4c7ac85960d1c66f
Justin Hooper <jhooperonline@gmail.com>
2025-04-28 10:49:33 -0600

    chore:  Cleanup beyond scope work in preparation of simplifying.

--- src/backend/data_specs/model_spec.py
+++
@@ -1,8 +0,0 @@
-import typing as t
-import pydantic as pyd
-import base_types as base
-
-class Model(pyd.BaseModel):
-  model_name: base.ShortName
-  api_key: base.ShortName | None
-  options: base.ConfigArguments
