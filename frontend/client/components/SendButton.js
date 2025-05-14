b5670d4817ddb29d92b9d337ade903d6a0f73612
Konstantin Balakirev <kbalakirev@gmail.com>
2025-05-14 17:11:12 -0500

    feat: Enhance CreateChatButton and SendButton components; add icon, improve styles, and update layout for better user experience

--- frontend/client/components/SendButton.js
+++ frontend/client/components/SendButton.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { IoArrowUpOutline } from 'react-icons/io5';
 
-export default function SendButton({ onClick, disabled = false }) {
+export default function SendButton({
+	onClick,
+	disabled = false,
+	isFullyExpanded,
+}) {
 	return (
-		<button
-			className='send-button'
+		<div
+			className={`send-button ${disabled ? 'inactive' : 'active'} ${
+				isFullyExpanded ? 'fade-in' : ''
+			}`}
 			onClick={onClick}
 			disabled={disabled}
-			style={{
-				backgroundColor: disabled ? '#ccc' : '#007bff',
-				color: 'white',
-				border: 'none',
-				borderRadius: '5px',
-				padding: '10px 20px',
-				cursor: disabled ? 'not-allowed' : 'pointer',
-				fontSize: '16px',
-			}}
+			// style={{
+			// 	color: disabled ? 'gray' : 'black',
+			// 	backgroundColor: disabled ? 'lightgrey' : 'white',
+			// 	border: disabled ? '1px solid white' : '1px solid gray',
+			// 	color: 'white',
+			// 	border: 'none',
+			// 	borderRadius: '5px',
+			// 	padding: '10px 20px',
+			// 	cursor: disabled ? 'not-allowed' : 'pointer',
+			// 	fontSize: '16px',
+			// }}
 		>
-			Send
-		</button>
+			<IoArrowUpOutline />
+		</div>
 	);
 }
