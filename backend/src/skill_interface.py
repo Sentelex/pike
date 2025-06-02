88a11e3b03c63e2520330842cf75b7feff1e71fd
Eden-Alem <edenberhanalem@gmail.com>
2025-06-02 09:26:27 -0600

    fix: make use of pydantic structure

--- backend/src/skill_interface.py
+++ backend/src/skill_interface.py
@@ -1,14 +1,22 @@
 from fastapi.responses import JSONResponse
 import src.graph_builder as gb
+from typing import List
+from src.models.skill import SkillInterface 
 
 
 def get_available_skills():
-    skills = [
-        {
-            "name": t.name,
-            "description": t.description,
-            "id": t.metadata["uuid"],
-            "icon": t.metadata["icon"]
-        } for t in gb.SKILL_LOOKUP.values()
+    skills: List[SkillInterface] = [
+        SkillInterface(
+            name=t.name,
+            description=t.description,
+            id=t.metadata["uuid"],
+            icon=t.metadata["icon"]
+        ) for t in gb.SKILL_LOOKUP.values()
     ]
-    return JSONResponse(content=skills)
+
+    response_data = [
+        {**skill.model_dump()}
+        for skill in skills
+    ]
+
+    return JSONResponse(content=response_data)
