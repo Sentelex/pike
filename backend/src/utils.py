9d8fe82ee914e4f3f6a08ec74855d00d0e83c9f8
Eden-Alem <edenberhanalem@gmail.com>
2025-06-09 14:16:58 -0600

    fix: remove tool_with_metadata decorator

--- backend/src/utils.py
+++
@@ -1,8 +0,0 @@
-import langchain_core.tools as lcct
-
-def tool_with_metadata(name: str, metadata: dict):
-    def wrapper(func):
-        tool = lcct.tool(name)(func)
-        tool.metadata = metadata
-        return tool
-    return wrapper
