8a4329a338b810483fea3a063ee9d591a2305d5e
Konstantin Balakirev <kbalakirev@gmail.com>
2025-06-18 16:24:25 -0500

    refactor: remove MarkdownDemo component and clean up AgentButton and Chat components

--- frontend/client/components/Chat.js
+++ frontend/client/components/Chat.js
@@ -10,7 +10,6 @@ import UserMessage from './UserMessage';
 import AgentMessage from './AgentMessage';
 import ChatMessageInput from './ChatMessageInput';
 import { smoothScrollTo } from '../utils/scroll';
-import MarkdownDemo from './MarkdownDemo'; // Assuming you have a MarkdownDemo component
 
 function Chat({ chatId, agentId, chatName, isOpen }) {
 	console.log('Chat component:', chatId, isOpen);
@@ -95,8 +94,6 @@ function Chat({ chatId, agentId, chatName, isOpen }) {
 							}
 					  })
 					: isOpen && <div>Loading chat history...</div>}
-
-				<MarkdownDemo />
 			</div>
 			{/* </div> */}
 			{isOpen && (
